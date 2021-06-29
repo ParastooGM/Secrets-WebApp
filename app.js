456caa1559daca61cccca1d5620885d84f85f40f
parastoo gol mohammadi <parastoogmohammadi@gmail.com>
2021-06-29 12:07:24 -0400

    Starter files

---
+++ app.js
@@ -0,0 +1,45 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const ejs = require("ejs");
+const mongoose = require("mongoose");
+
+const app = express();
+
+app.use(express.static("public"));
+app.set('view engine', 'ejs');
+app.use(bodyParser.urlencoded({ extended: true}));
+
+mongoose.connect("mongodb://localhost:27017/userDB", {useNewUrlParser: true ,  useUnifiedTopology: true });
+
+const userSchema ={
+  email: String,
+  password: String
+};
+
+const User = new mongoose.model("User", userSchema);
+
+app.get("/", function(req, res){
+  res.render("home");
+});
+
+app.get("/login", function(req, res){
+  res.render("login");
+});
+
+app.get("/register", function(req, res){
+  res.render("register");
+});
+
+app.post("/register", function(req, res){
+
+});
+
+app.get("/secrets", function(req, res){
+  res.render("secrets");
+});
+
+app.get("/submit", function(req, res){
+  res.render("submit");
+});
+
+app.listen(3000);
---
+++ public/index.js
@@ -0,0 +1,13 @@
+
+setTimeout(function(){
+  $('<img class="cloud-image1" src="images/cloud1.gif" height="50" width="70">').appendTo($('.cl1'));
+}, 1514);
+setTimeout(function(){
+  $('<img class="cloud-image2" src="images/cloud2.gif" height="50" width="70">').appendTo($('.cl2'));
+}, 948);
+setTimeout(function(){
+  $('<img class="cloud-image3" src="images/cloud3.gif" height="50" width="70">').appendTo($('.cl3'));
+}, 2229);
+setTimeout(function(){
+  $('<img class="cloud-image4" src="images/cloud4.gif" height="50" width="70">').appendTo($('.cl4'));
+}, 676);
