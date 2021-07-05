a17553c3453198eb9b7583df2153024cb98a220a
parastoo gol mohammadi <parastoogmohammadi@gmail.com>
2021-07-05 17:45:09 -0400

    setting up login, register, database and bcrypt with style adjustments.

--- app.js
+++ app.js
@@ -2,6 +2,8 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
 const mongoose = require("mongoose");
+const bcrypt = require("bcrypt");
+const saltRounds = 12;
 
 const app = express();
 
@@ -26,11 +28,43 @@ app.get("/login", function(req, res){
   res.render("login");
 });
 
+app.post("/login", function(req, res){
+  const username = req.body.username;
+  const password = req.body.password;
+
+  User.findOne({email: username}, function(err, foundUser){
+    if(err){
+      console.log(err);
+    }else{
+      if (foundUser){
+      bcrypt.compare(password, foundUser.password, function(err, result){
+        if (result === true){
+          res.render("secrets");
+        }
+      });
+    };
+  };
+});
+});
+
 app.get("/register", function(req, res){
   res.render("register");
 });
 
 app.post("/register", function(req, res){
+
+  bcrypt.hash( req.body.password, saltRounds, function(err, hash){
+    const newUser = new User({
+      email: req.body.username,
+      password: hash
+    });
+    newUser.save(function(err){
+      if (err){
+        console.log(err);
+      }else{
+        res.render("secrets");
+      }  });
+  });
 
 });
 
