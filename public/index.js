fd2e222de31493efc243dd8961a95944a1c62be5
parastoo gol mohammadi <parastoogmohammadi@gmail.com>
2021-07-13 16:00:19 -0400

    responsive style

--- public/index.js
+++ public/index.js
@@ -1,13 +1,10 @@
-
+//dynamically adding chat cloud gifs to the home page.
 setTimeout(function(){
-  $('<img class="cloud-image1" src="images/cloud1.gif" height="50" width="70">').appendTo($('.cl1'));
+  $('<img class="cloud-image1" src="images/cloud1.gif" height="50" width="70">').appendTo($('.right3'));
 }, 1514);
 setTimeout(function(){
-  $('<img class="cloud-image2" src="images/cloud2.gif" height="50" width="70">').appendTo($('.cl2'));
+  $('<img class="cloud-image2" src="images/cloud2.gif" height="50" width="70">').appendTo($('.right2'));
 }, 948);
 setTimeout(function(){
-  $('<img class="cloud-image3" src="images/cloud3.gif" height="50" width="70">').appendTo($('.cl3'));
+  $('<img class="cloud-image3" src="images/cloud3.gif" height="50" width="70">').appendTo($('.right4'));
 }, 2229);
-setTimeout(function(){
-  $('<img class="cloud-image4" src="images/cloud4.gif" height="50" width="70">').appendTo($('.cl4'));
-}, 676);
